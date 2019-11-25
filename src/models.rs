817642fa0f7b9d94de9da98d4e4e9079a6679de9
Dawid J. Kubis <dawidk.kubis3@gmail.com>
2019-11-25 17:01:57 +0100

    model updated - still gay tho

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,11 @@
 #[derive(Queryable)]
 pub struct Reservation {
+    id: u32,
+    name: String,
+    description: String,
+    rooms: u16,
+    begin_time: String,
+    end_time: String,
+    layout: u16,
+    approved: bool,
 }
