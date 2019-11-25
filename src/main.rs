8af6ab3ae0d672e1c65340494dd38e37e08126ca
Dawid J. Kubis <dawidk.kubis3@gmail.com>
2019-11-25 13:05:32 +0100

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> String{
+    "index".to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![hello])
+        .launch();
 }
