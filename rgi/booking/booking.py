dd664af5f5ae235dc37793ae43daa765163ab396
lesves <lukas.veskrna@gmail.com>
2019-11-25 15:48:55 +0100

    booking.py edits

--- rgi/booking/booking.py
+++ rgi/booking/booking.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+import sys
+
 
 Base = declarative_base()
 
 class Booking(Base):
-	__tablename__ = "booking"
+    __tablename__ = "booking"
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
@@ -14,3 +16,23 @@ class Booking(Base):
     end = Column(DateTime, nullable=False)
     layout = Column(Integer, nullable=True)
     approved = Column(Boolean, nullable=False)
+
+
+def get(data):
+    ...
+def post(data):
+    ...
+def patch(data):
+    ...
+def delete(data):
+    ...
+
+methods = {"get": get, "post": post, "patch": patch, "delete": delete}
+
+
+if __name__ == "__main__":
+    data = json.load(sys.stdin)
+    if len(sys.argv) < 2:
+        methods["get"](data)
+    else:
+        methods[sys.argv[1]](data)
