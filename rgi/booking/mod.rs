6e7028ce4ad5f927ee5a291124f96fd519ebc629
lesves <lukas.veskrna@gmail.com>
2019-11-26 12:11:41 +0100

    added other weird methodz

--- rgi/booking/mod.rs
+++ rgi/booking/mod.rs
@@ -13,13 +13,34 @@ pub fn get(id: i32) -> String {
 
 #[post("/booking", format = "application/json", data = "<_input>")]
 pub fn post(_input: Json<NewReservation>) -> String {
-	 unimplemented!()
-	 /* rgi! { ... data: serde_json::to_string(_input).unwrap() } */
+	rgi! {
+		POST "rgi/booking/booking.py"
+		data: serde_json::to_string(_input).unwrap()
+	}
+}
+
+#[patch("/booking/<id>", format = "application/json", data = "<_input>")]
+pub fn post(id: i32, _input: Json<NewReservation>) -> String {
+	rgi! {
+		PATCH "rgi/booking/booking.py"
+		arg: id
+		data: serde_json::to_string(_input).unwrap()
+	}
+}
+
+#[delete("/booking/<id>", format = "application/json")]
+pub fn get(id: i32) -> String {
+	rgi! {
+		DELETE "rgi/booking/booking.py"
+		arg: id
+	}
 }
 
 pub fn routes() -> Vec<Route> {
 	routes![
 		get,
 		post,
+		patch,
+		delete
 	]
 } 
