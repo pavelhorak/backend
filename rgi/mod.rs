9ff5156a39332cf188fdca4dede2038557354b67
Lukáš Hozda <luk.hozda@gmail.com>
2019-11-26 14:10:12 +0100

    minor fixes pt.2

--- rgi/mod.rs
+++ rgi/mod.rs
@@ -20,10 +20,14 @@ macro_rules! rgi {
 			let _ = writeln!(stdin, "\t\"args\": {{");
 			$(let _ = writeln!(stdin, "\t\t\"{}\": \"{}\",", stringify!($arg), ($arg).to_string());)*
 			let _ = writeln!(stdin, "\t}},");
-			$(let _ = writeln!(stdin, "\t\"data\": {}", serde_json::to_string($data)
-				.unwrap()
-				.replace("{\"", "{")
-				.replace("\"}", "}"));)?
+			$(let _ = writeln!(stdin, "\t\"data\": {}",
+				{
+					let mut s = serde_json::to_string($data).unwrap();
+					s = s.chars().skip(1).collect();
+					s.pop();
+					s
+				});
+			)?
 			let _ = writeln!(stdin, "}}");
 		}
 
