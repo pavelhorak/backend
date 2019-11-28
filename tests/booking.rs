6164a30975d82f4ee875618cdbb76a13ded6c158
lesves <lukas.veskrna@gmail.com>
2019-11-28 14:38:18 +0100

    stuff

--- tests/booking.rs
+++ tests/booking.rs
@@ -13,7 +13,7 @@ pub fn test_booking() {
 	req.set_body(r#"{ "name": "test", "description": "test stuff", "author": "hozdic", "begin_time": "2019-12-12T12:30", "end_time": "2019-12-13T13:25", "rooms": 3, "layout": 0, "people": 30}"#);
 	assert_eq!(req.dispatch().body_string().unwrap(), r#"{"result": 0, "id": 1}"#);
 
-	let v: backend::db::Reservation = serde_json::from_str(&cli.get("/rgi/events/1").dispatch().body_string().unwrap()).unwrap();
+	let _v: backend::db::Reservation = serde_json::from_str(&cli.get("/rgi/events/1").dispatch().body_string().unwrap()).unwrap();
 	//assert_eq!(v, );
 	cli.delete("/rgi/events/1").header(ContentType::JSON).dispatch();
 	assert_eq!(cli.get("/rgi/events/").dispatch().body_string().unwrap(), "[]");
