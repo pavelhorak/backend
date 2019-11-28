e7db7c0ebc43c0fe174e606bcc6226612850f29e
lesves <lukas.veskrna@gmail.com>
2019-11-28 16:16:35 +0100

    better style

--- tests/booking.rs
+++ tests/booking.rs
@@ -13,8 +13,18 @@ pub fn test_booking() {
 	req.set_body(r#"{ "name": "test", "description": "test stuff", "author": "hozdic", "begin_time": "2019-12-12T12:30", "end_time": "2019-12-13T13:25", "rooms": 3, "layout": 0, "people": 30}"#);
 	assert_eq!(req.dispatch().body_string().unwrap(), r#"{"result": 0, "id": 1}"#);
 
-	let _v: backend::db::Reservation = serde_json::from_str(&cli.get("/rgi/events/1").dispatch().body_string().unwrap()).unwrap();
-	//assert_eq!(v, );
+	let v: backend::db::Reservation = serde_json::from_str(&cli.get("/rgi/events/1").dispatch().body_string().unwrap()).unwrap();
+	assert_eq!(v.approved, 0);
+
+	cli.post("/rgi/events/1/approve/").header(ContentType::JSON).dispatch();
+
+	let v: backend::db::Reservation = serde_json::from_str(&cli.get("/rgi/events/1").dispatch().body_string().unwrap()).unwrap();
+	assert_eq!(v.approved, 1);
+
+	let mut req = cli.post("/rgi/events/").header(ContentType::JSON);
+	req.set_body(r#"{ "name": "test2", "description": "test stuff2", "author": "hozdic2", "begin_time": "2019-12-12T11:30", "end_time": "2019-12-13T13:25", "rooms": 3, "layout": 0, "people": 100}"#);
+	assert_eq!(req.dispatch().body_string().unwrap(), r#"{"result": 2}"#);
+
 	cli.delete("/rgi/events/1").header(ContentType::JSON).dispatch();
 	assert_eq!(cli.get("/rgi/events/").dispatch().body_string().unwrap(), "[]");
 }
