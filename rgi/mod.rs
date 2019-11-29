682e74e155d878b2eb7f840383e7b56435ded395
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-11-29 20:06:35 +0100

    got rid of rgi macro

--- rgi/mod.rs
+++ rgi/mod.rs
@@ -6,60 +6,6 @@
 
 use rocket::Route;
 
-/// makro, které vygeneruje boilerplate pro volání daného rgi
-///
-/// syntaxe (hranaté závorky značí, že parametr není povinný):
-/// ```no_run
-/// rgi! {
-///     HTTP_METODA "rgi/cesta/k/rgi/binarce"
-///     [arg: identifikátor]* // argumentem se myslí parametr z URL
-///     [data: <vyraz>]? // někdy je potřeba obalit do závorek ()
-/// }
-/// ```
-/// (všechny argumenty a data musí implementovat `serialize` ze `serde`)
-///
-/// příklad:
-/// rgi! {
-///     GET "rgi/lol/lol.py"
-///     arg: name
-///     arg: password
-///     data: (Objekt)
-/// }
-#[macro_export]
-macro_rules! rgi {
-	{$method:ident $name:literal $(arg: $arg:ident),*  $(data: $data:tt)? } => {
-		{
-			// we need this lint to avoid warnings caused by extra parens
-			// which are not extra since they help the parser
-			#![allow(unused_parens)]
-			#[allow(unused_imports)]
-			use std::process::{Command, Stdio};
-			#[allow(unused_imports)]
-			use std::io::{Read, Write};
-
-			let mut cmd = Command::new($name)
-				.stdin(Stdio::piped())
-				.stdout(Stdio::piped())
-				.arg(stringify!($method))
-				.spawn()
-				.expect("kinda gay");
-
-			if let Some(ref mut stdin) = &mut cmd.stdin {
-				let _ = writeln!(stdin, "{{");
-				let _ = writeln!(stdin, "\t\"args\": {{");
-				$(let _ = writeln!(stdin, "\t\t\"{}\": \"{}\",", stringify!($arg), ($arg).to_string());)*
-				let _ = writeln!(stdin, "\t}},");
-				$(let _ = writeln!(stdin, "\t\"data\": {}", serde_json::to_string($data).unwrap());)?
-				let _ = writeln!(stdin, "}}");
-			}
-
-			let cmd = cmd.wait_with_output().unwrap();
-
-			String::from_utf8_lossy(&cmd.stdout).to_string()
-		}
-	}
-}
-
 /// modul obsahující endpointy pro CRUD na rezervaci
 pub mod booking;
 
