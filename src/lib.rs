150ff822b732d938fa7f5cb9456e30e52862054b
Lukáš Hozda <luk.hozda@gmail.com>
2019-11-30 17:18:59 +0100

    more appropriate rename

--- src/lib.rs
+++ src/lib.rs
@@ -56,7 +56,7 @@ use rocket::http::Method;
 use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
 pub mod auth;
-pub mod rgi;
+pub mod api;
 pub mod db;
 pub mod static_server;
 
@@ -88,7 +88,7 @@ pub fn init() -> rocket::Rocket {
 	rocket::ignite()
 		.register(catchers![static_server::not_found])
 		.mount("/", routes![static_server::index, static_server::frontend, static_server::favicon, auth::me])
-		.mount("/rgi/", rgi::routes())
+		.mount("/api/", api::routes())
 		.attach(cors)
 		.attach(DbConn::fairing())
 }
